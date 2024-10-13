3bb1d25816501f24bf8ad2ad3599327c6ff6ae39
Emerbsilva <ebsindustrial@gmail.com>
2024-10-13 12:19:49 -0300

    alterado - inserido texto recuperar na string

--- app/src/main/java/com/ebsindustrial/bimobiliario/MainActivity.kt
+++ app/src/main/java/com/ebsindustrial/bimobiliario/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.ebsindustrial.bimobiliario.databinding.ActivityMainBinding
 import kotlin.random.Random
@@ -136,7 +135,9 @@ class MainActivity : AppCompatActivity() {
     //
     private fun recuperar(){
         binding.recover.setOnClickListener {
+            habilita()
             getData()
+            binding.txtSorte.setText(R.string.recupera)
         }
     }
     //
